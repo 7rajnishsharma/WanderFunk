2a446aed95d428217b0c6ba0981b0379c0a64087
Rajnish Sharma <111423953+7rajnishsharma@users.noreply.github.com>
2023-09-08 19:02:48 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Footer() {
+  return (
+    <>
+
+    <div className="footer">
+        <div className="footer-bottom">
+            <p>Created by <Link to="https://7rajnishsharma.github.io/MyPortfolio/" target='_blank'>Rajnish Kumar Sharma</Link> </p>
+        </div>
+    </div>
+    
+    
+    </>
+  )
+}
